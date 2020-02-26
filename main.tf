4060f2baf350218fb938b9152ace927f8bcb1d4e
Marcelo <marcelo@marceloweb.info>
2020-02-26 01:10:07 -0300

    - Cluster with the default node pool

--- main.tf
+++ main.tf
@@ -2,9 +2,7 @@ resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = "us-central1"
   project  = "lab-marceloweb"
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count       = 2
 
   master_auth {
     username = ""
@@ -14,26 +12,7 @@ resource "google_container_cluster" "primary" {
       issue_client_certificate = false
     }
   }
+
 }
 
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "my-node-pool"
-  location   = "us-central1"
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
-
-  node_config {
-    preemptible  = true
-    machine_type = "n1-standard-1"
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
-}
 
